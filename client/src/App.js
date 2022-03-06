5e178bd7a2aeebe2a1c6fcaed1b5065a22b6e24d
Iris Law <irislawly@gmail.com>
2022-03-05 21:26:45 -0800

    worked on adding db

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,60 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, doc } from 'firebase/firestore/lite';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI56fFPY0z2aRtIV3LqqHAlJ-0SLCtcEo",
+  authDomain: "shoplocal-d5448.firebaseapp.com",
+  projectId: "shoplocal-d5448",
+  storageBucket: "shoplocal-d5448.appspot.com",
+  messagingSenderId: "54898528983",
+  appId: "1:54898528983:web:ab516f71bf91751aca9692"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+console.log(db);
+
+
+var citiesRef = collection(db,"cities");
+console.log(citiesRef);
+citiesRef.doc("SF").set({
+    name: "San Francisco", state: "CA", country: "USA",
+    capital: false, population: 860000,
+    regions: ["west_coast", "norcal"] });
+citiesRef.doc("LA").set({
+    name: "Los Angeles", state: "CA", country: "USA",
+    capital: false, population: 3900000,
+    regions: ["west_coast", "socal"] });
+citiesRef.doc("DC").set({
+    name: "Washington, D.C.", state: null, country: "USA",
+    capital: true, population: 680000,
+    regions: ["east_coast"] });
+citiesRef.doc("TOK").set({
+    name: "Tokyo", state: null, country: "Japan",
+    capital: true, population: 9000000,
+    regions: ["kanto", "honshu"] });
+citiesRef.doc("BJ").set({
+    name: "Beijing", state: null, country: "China",
+    capital: true, population: 21500000,
+    regions: ["jingjinji", "hebei"] });
+
+// Get a list of cities from your database
+/*
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+*/
 
 class App extends Component {
     constructor(props) {
@@ -9,10 +63,12 @@ class App extends Component {
     }
 
     callAPI() {
+       // fetch(getCities(db)).then(res => console.log(res.text))
         fetch("http://localhost:9000/testAPI")
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
+            
     }
 
     componentDidMount() {
